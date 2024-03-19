819388042dc7fd1bf706f23c3035cabf0aeaee1b
Abdelrahmane Bekhli <abdelrahmane001@gmail.com>
2024-03-19 14:48:58 +0000

    Fixed getDistance, added a method to calculate hash and return it in hex value format

--- src/HashID.java
+++ src/HashID.java
@@ -25,21 +25,59 @@ public class HashID {
 		}
 	}
 
-	public static int getDistance(byte[] hash1, byte[] hash2) {
-		byte b1 = 0;
-		byte b2 = 0;
+	public static int getDistance(String value1, String value2) {
+
+		String v1 = hexHash(value1);
+		String v2 = hexHash(value2);
+		char char1 = ' ';
+		char char2 = ' ';
 		int counter = 0;
-		for (int i = 0; i < hash1.length; i++) {
-			if (hash1[i] != hash2[i]) {
-				b1 = hash1[i];
-				b2 = hash2[i];
+
+		for (int i = 0; i < v1.length(); i++) {
+			if (v1.charAt(i) != v2.charAt(i)) {
+				char1 = v1.charAt(i);
+				char2 = v2.charAt(i);
 				break;
 			} else {
 				counter++;
 			}
 		}
-		byte res = (byte)((b1 ^ b2));
-		int numberDiff = numberOfLeadingZeros(res);
-		return 256 - ((counter * 8) - 24 + numberDiff);
+		if (char1 != ' ' & char2 != ' ') {
+			// Convert hexadecimal characters to their integer values
+			int intChar1 = Character.digit(char1, 16);
+			int intChar2 = Character.digit(char2, 16);
+
+			// Perform XOR operation
+			int result = intChar1 ^ intChar2;
+			StringBuilder binaryResult = new StringBuilder(Integer.toBinaryString(result));
+			int zeros = 0;
+			while (binaryResult.length() < 4) {
+				binaryResult.insert(0, "0");
+				zeros++;
+			}
+			return 256 - ((counter * 4) + zeros);
+
+		}
+		return 0;
+	}
+
+	public static String hexHash(String val){
+		StringBuilder hexString = new StringBuilder();
+		try {
+			// Compute the hash of the input string 'val' using the 'computeHashID' method
+			byte[] hashedData = HashID.computeHashID(val);
+			for (byte b : hashedData) {
+				// Convert the byte to a hexadecimal string
+				String hex = Integer.toHexString(0xff & b);
+				//if byte value < 16
+				if (hex.length() == 1) {
+					hexString.append('0');
+				}
+				hexString.append(hex);
+			}
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+		return hexString.toString();
 	}
 }
