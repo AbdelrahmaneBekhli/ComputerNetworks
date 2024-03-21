f16f166d7d94740a36834824a39e54310203a40d
Abdelrahmane Bekhli <abdelrahmane001@gmail.com>
2024-03-21 20:47:32 +0000

    Added address as attributes

--- src/NodeInfo.java
+++ src/NodeInfo.java
@@ -3,25 +3,25 @@ class NodeInfo {
     private final String nodeName;
     private final int port;
     private final String StartTime;
+    private final String address;
 
-    public NodeInfo(int ID, String nodeName, int port, String startTime) {
+    public NodeInfo(int ID, String nodeName, int port, String startTime, String address) {
         this.ID = ID;
         this.nodeName = nodeName;
         this.port = port;
         this.StartTime = startTime;
+        this.address = address;
     }
 
     public String getNodeName() {
         return nodeName;
     }
-
-    public int getPort() {
-        return port;
+    public String getAddress() {
+        return address;
     }
     public String getStartTime() {
         return StartTime;
     }
-    public int getID() {
-        return ID;
-    }
+    public int getPort() {return port;}
+    public int getID() {return ID;}
 }
