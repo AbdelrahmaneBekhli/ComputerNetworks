4db997a9a262ce457655f47715d97de98605299d
Abdelrahmane Bekhli <abdelrahmane001@gmail.com>
2024-03-31 13:45:22 +0100

    added prints for testing

--- src/FullNode.java
+++ src/FullNode.java
@@ -70,23 +70,24 @@ public class FullNode implements FullNodeInterface {
         for (int port = 20000; port <= 20300; port++) {
             if (port != portNumber & !(checkUsedPort(port))) {
                 try {
+                    System.out.println("establishing connection to: " + IpAddress + " port: " + port);
                     // Create a socket and attempt to connect to the target host and port
                     Socket socket = new Socket();
                     socket.connect(new InetSocketAddress(IpAddress, port), 5);
                     // Initialize reader and writer
                     BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
                     BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    System.out.println("establishing connection to: ip address: " + IpAddress + " port: " + port);
+                    System.out.println("sending START to: " + IpAddress + " port: " + port);
 
 
                     //checks if the connection was successful
                     if(sendStart(reader, writer)) {
                         if (notifyNode(reader, writer)) {
-                            System.out.println("connected to: ip address: " + IpAddress + " port: " + port);
+                            System.out.println("Success to: " + IpAddress + " port: " + port);
                             updateNetworkMap(socket,connectingNode, port, IpAddress+":"+port);
                         }
                     } else{
-                        System.out.println("Not connected to ip address: " + IpAddress + " port: " + port);
+                        System.out.println("FAILED connected to: " + IpAddress + " port: " + port);
                     }
                 } catch (Exception e) {
                     //ignore offline nodes
